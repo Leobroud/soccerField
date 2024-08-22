9444262d0f7e3f70cf8f6785518c517468b758aa
topd97 <topd97@yahoo.com.br>
2024-08-22 16:29:12 -0300

    Adding names

--- soccerField/Coordinator.swift
+++ soccerField/Coordinator.swift
@@ -15,6 +15,7 @@ struct PlayerModel {
     let id: Int
     let position: SIMD3<Float>
     let positionName: String
+    let name: String
 }
 
 
@@ -24,43 +25,30 @@ class Coordinator: NSObject, ARSessionDelegate {
     
     var arView: ARView?
     
-    let goalKeeperPosition = SIMD3<Float>(0, 0.005, -0.3) // Goleiro
-    
-    let playerPositions: [SIMD3<Float>] = [
-                SIMD3<Float>(-0.15, 0.005, -0.15), // Zagueiro esquerdo
-                SIMD3<Float>( 0.15, 0.005,  -0.15), // Zagueiro direito
-                SIMD3<Float>(-0.05, 0.005,  -0.2), // Zagueiro central esquerdo
-                SIMD3<Float>( 0.05, 0.005,  -0.2), // Zagueiro central direito
-                SIMD3<Float>(-0.15, 0.005,  0.05), // Meio-campista esquerdo
-                SIMD3<Float>(-0.05, 0.005,  -0.05), // Meio-campista direita
-                SIMD3<Float>( 0.05, 0.005,  -0.05), // Meio-campista meio esquerda
-                SIMD3<Float>( 0.15, 0.005,  0.05), // Meio-campista meio direita
-                SIMD3<Float>( -0.08, 0.005, 0.2), // Atacante esquerdo
-                SIMD3<Float>( 0.08, 0.005,  0.2), // Atacante central
-            ]
-    let playerPositionNames: [String] = [
-                "Zagueiro esquerdo",
-                "Zagueiro direito",
-                "Zagueiro central esquerdo",
-                "Zagueiro central direito",
-                "Meio-campista esquerdo",
-                "Meio-campista central esquerda",
-                "Meio-campista central direita",
-                "Meio-campista direita",
-                "Atacante esquerdo",
-                "Atacante direita"
-            ]
-    
+    let goleiro = PlayerModel(id: 0, position: SIMD3<Float>(0, 0.005, -0.3), positionName: "Goleiro", name: "Diego Alves")
+    let players: [PlayerModel] = [
+        PlayerModel(id: 1, position: SIMD3<Float>(-0.15, 0.005, -0.15), positionName: "Zagueiro esquerdo", name: "Ayrton Lucas"),
+        PlayerModel(id: 2, position: SIMD3<Float>(0.15, 0.005, -0.15), positionName: "Zagueiro direito", name: "Varela"),
+        PlayerModel(id: 3, position: SIMD3<Float>(-0.05, 0.005, -0.2), positionName: "Zagueiro central esquerdo", name: "Léo Pereira"),
+        PlayerModel(id: 4, position: SIMD3<Float>(0.05, 0.005, -0.2), positionName: "Zagueiro central direito", name: "Fabricio Bruno"),
+        PlayerModel(id: 5, position: SIMD3<Float>(-0.15, 0.005, 0.05), positionName: "Meio-campista esquerdo", name: "Cebolinha"),
+        PlayerModel(id: 6, position: SIMD3<Float>(-0.05, 0.005, -0.05), positionName: "Meio-campista central esquerda", name: "De la Cruz"),
+        PlayerModel(id: 7, position: SIMD3<Float>(0.05, 0.005, -0.05), positionName: "Meio-campista central direita", name: "Pulgar"),
+        PlayerModel(id: 8, position: SIMD3<Float>(0.15, 0.005, 0.05), positionName: "Meio-campista direita", name: "Luiz Araújo"),
+        PlayerModel(id: 9, position: SIMD3<Float>(-0.08, 0.005, 0.2), positionName: "Atacante esquerdo", name: "Pedro"),
+        PlayerModel(id: 10, position: SIMD3<Float>(0.08, 0.005, 0.2), positionName: "Atacante central", name: "Arrascaeta")
+    ]
     
     var fieldEntity: ModelEntity?
     var playersEntities: [(PlayerModel, ModelEntity)] = []
+    var anchor: AnchorEntity?
     
     func setup() {
         
         guard let arView = arView else { return }
         
         // Carregar o modelo do campo de futebol
-        let anchor = AnchorEntity(plane: .horizontal)
+        self.anchor = AnchorEntity(plane: .horizontal)
         
         guard let fieldModelEntity = try? ModelEntity.loadModel(named: "soccer") else {
             fatalError("Erro to build modelEntity")
@@ -70,35 +58,20 @@ class Coordinator: NSObject, ARSessionDelegate {
         fieldModelEntity.scale = SIMD3<Float>(repeating: 0.004)
         
         // Adicionar o campo à âncora
-        anchor.addChild(fieldModelEntity)
+        self.anchor?.addChild(fieldModelEntity)
         fieldEntity = fieldModelEntity
         
-        // Ajustar a escala do campo
-        
         // Add goleiro
-        guard let playerModelEntity = try? ModelEntity.loadModel(named: "flamengo_goleiro") else {
-            fatalError("Erro to build modelEntity")
-        }
-        playerModelEntity.scale = SIMD3<Float>(repeating: 0.05)
-        playerModelEntity.position = goalKeeperPosition
-        fieldModelEntity.addChild(playerModelEntity, preservingWorldTransform: true)
-        playersEntities.append((PlayerModel(id: 0, position: goalKeeperPosition, positionName: "Goleiro"), playerModelEntity))
+        createPlayer(goleiro, field: fieldModelEntity)
         
         // Add Players
-        for index in 0..<playerPositions.count {
-            guard let playerModelEntity = try? ModelEntity.loadModel(named: "flamengo") else {
-                fatalError("Erro to build modelEntity")
-            }
-            playerModelEntity.scale = SIMD3<Float>(repeating: 0.05)
-            playerModelEntity.position = playerPositions[index]
-            fieldModelEntity.addChild(playerModelEntity, preservingWorldTransform: true)
-            playersEntities.append((PlayerModel(id: index+1, position: playerPositions[index], positionName: playerPositionNames[index]), playerModelEntity))
+        for player in players {
+            createPlayer(player, field: fieldModelEntity)
         }
         
-        
-        anchor.generateCollisionShapes(recursive: true)
+        self.anchor?.generateCollisionShapes(recursive: true)
         // Adicionar a âncora à cena
-        arView.scene.anchors.append(anchor)
+        arView.scene.anchors.append(self.anchor ?? AnchorEntity(plane: .horizontal))
     }
     
     @objc func rotateModel(_ gesture: UIPanGestureRecognizer) {
@@ -129,41 +102,26 @@ class Coordinator: NSObject, ARSessionDelegate {
             if hit.first?.entity == fieldEntity {
                 print("[debug] tocou no campo")
             } else {
-                for player in playersEntities {
-                    if hit.first?.entity == player.1 {
-                        print("[debug] tocou no jogador ", player.0.id)
-                        let material: Material = SimpleMaterial(color: .systemPink, isMetallic: true)
-                        guard let cardModelEntity = try? ModelEntity(mesh: MeshResource.generateBox(size: 1.0), materials: [material]) else {
-                            fatalError("Erro to build modelEntity")
-                        }
-                        
-                        print("[debug] fieldEntity:", fieldEntity?.position)
-                        
-                        cardModelEntity.scale = SIMD3<Float>(repeating: 0.05)
-                        cardModelEntity.position = goalKeeperPosition
-                        fieldEntity?.addChild(cardModelEntity, preservingWorldTransform: true)
-                        
-                    }
-                }
+                
+//                for player in playersEntities {
+//                    if hit.first?.entity == player.1 {
+//                        print("[debug] tocou no jogador ", player.0.id)
+//                        let material: Material = SimpleMaterial(color: .systemPink, isMetallic: true)
+//                        guard let cardModelEntity = try? ModelEntity(mesh: MeshResource.generateBox(size: 1.0), materials: [material]) else {
+//                            fatalError("Erro to build modelEntity")
+//                        }
+//                        
+//                        
+//                        print("[debug] fieldEntity:", arView.scene.anchors)
+//                        
+//                        cardModelEntity.scale = SIMD3<Float>(repeating: 0.05)
+//                        cardModelEntity.position = goalKeeperPosition
+//                        self.anchor?.addChild(cardModelEntity, preservingWorldTransform: true)
+//                        
+//                    }
+//                }
             }
         }
-        
-//        switch gesture.state {
-//        case .began:
-//            lastPanLocation = gesture.location(in: arView)
-//        case .changed:
-//            let currentPanLocation = gesture.location(in: arView)
-//            let deltaX = Float(currentPanLocation.x - lastPanLocation.x) * 0.01 // Fator de rotação horizontal
-//            
-//            if let entity = arView.scene.anchors.first?.children.first {
-//                let rotation = simd_quatf(angle: deltaX, axis: [0, 0, 0.5])
-//                entity.transform.rotation *= rotation
-//            }
-//            
-//            lastPanLocation = currentPanLocation
-//        default:
-//            break
-//        }
     }
     
     @objc func scaleModel(_ gesture: UIPinchGestureRecognizer) {
@@ -180,5 +138,30 @@ class Coordinator: NSObject, ARSessionDelegate {
         default:
             break
         }
+    }
+}
+
+// MARK: Helpers
+extension Coordinator {
+    private func createPlayer(_ player: PlayerModel, field: ModelEntity) {
+        guard let playerModelEntity = try? ModelEntity.loadModel(named: "flamengo") else {
+            fatalError("Erro to build modelEntity")
+        }
+        playerModelEntity.scale = SIMD3<Float>(repeating: 0.05)
+        playerModelEntity.position = player.position
+        field.addChild(playerModelEntity, preservingWorldTransform: true)
+        playersEntities.append((player, playerModelEntity))
+        
+        let material: Material = SimpleMaterial(color: .red, isMetallic: false)
+        guard let cardModelEntity = try? ModelEntity(mesh: MeshResource.generateText(player.name, font: .systemFont(ofSize: 14), alignment: .center), materials: [material]) else {
+            fatalError("Erro to build modelEntity")
+        }
+        
+        cardModelEntity.scale = SIMD3<Float>(repeating: 0.0015)
+        var position = player.position
+        position.y = 0.01
+        position.x -= 0.05
+        cardModelEntity.position = position
+        field.addChild(cardModelEntity, preservingWorldTransform: true)
     }
 }
